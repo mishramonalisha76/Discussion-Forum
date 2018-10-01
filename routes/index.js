b032ab6c991414e57be8f31274da7f2ff85f7278
Rajashree Parhi <rajashreeparhi23@gmail.com>
2018-10-01 21:01:40 +0530

    files added

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,28 @@
 var express = require('express');
 var router = express.Router();
-
+var c=0;
+var io = require('socket.io').listen(3001, {log: false});
 /* GET home page. */
-router.get('/', function(req, res) {
-  res.render('index', { title: 'Express' });
+router.get('/' ,function(req,res){
+  res.redirect('home');
 });
+router.get('/login' ,function(req,res){
+  res.render('login');
+})
+router.get('/chat' ,function(req,res){
+  res.render('chatbox');
+})
 
+
+router.post('/homepage',function(req,res){
+  res.redirect('/login');
+
+});
+router.get('/homepage' ,function(req,res){
+  res.render('page');
+})
+router.post('/login',function(req,res){
+  res.redirect('/chat');
+
+});
 module.exports = router;
