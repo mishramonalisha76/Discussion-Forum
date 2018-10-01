931723ad206f330549599299fdf614535da6dcb4
agnipanda <pandaagni@outlook.com>
2018-10-01 22:06:11 +0530

    updated homepage and routes

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/' ,function(req,res){
-  res.redirect('home');
+  res.redirect('homepage');
 });
 router.get('/login' ,function(req,res){
   res.render('login');
@@ -13,23 +13,13 @@ router.get('/register' ,function(req,res){
 })
 router.get('/chat' ,function(req,res){
   res.render('chatbox');
-})
-
-
-router.post('/homepage/login',function(req,res){
-  res.redirect('/login');
-
-
 });
-router.post('/homepage/register',function(req,res){
-  res.redirect('/register');
 
-});
+
 router.get('/homepage' ,function(req,res){
   res.render('page');
 })
 router.post('/login',function(req,res){
   res.redirect('/chat');
-
 });
 module.exports = router;
