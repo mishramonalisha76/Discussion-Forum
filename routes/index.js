8a3ce1cd5b1fd7e8678d0b6536e74e406e6f1205
agnipanda <pandaagni@outlook.com>
2018-10-01 21:06:33 +0530

    removed socket.io

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,6 @@
 var express = require('express');
 var router = express.Router();
-var c=0;
-var io = require('socket.io').listen(3001, {log: false});
+
 /* GET home page. */
 router.get('/' ,function(req,res){
   res.redirect('home');
